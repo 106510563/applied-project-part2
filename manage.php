b0d4a97cc00ddd47606dea70326543acef9d9ea8
Paul Harrington <106578516@student.swin.edu.au>
2026-05-25 15:09:21 +1000

    added nav, html meta tags

--- manage.php
+++ manage.php
@@ -1,11 +1,26 @@
-        <link href="styles/styles.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Management</title>
+</head>
+<body>
+
+<link href="styles/styles.css" rel="stylesheet">
 
 <?php
 include "inc_files/header.inc";
+include "inc_files/nav.inc";
 ?>
+
+
 
 
 <?php
 include "inc_files/footer.inc";
 ?>
 
+        
+</body>
+</html>
