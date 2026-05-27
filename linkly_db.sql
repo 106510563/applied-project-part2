46c1a8180b15d3c277d5fcc53feac4a750c5f710
Cheese34-SSD <106503741@student.swin.edu.au>
2026-05-27 17:51:57 +1000

    starting the table

--- linkly_db.sql
+++ linkly_db.sql
@@ -77,21 +77,10 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Table structure for table `manage` (meant for manage.php)
+-- Data for `manage` account for access to manage.php
 --
 
-CREATE TABLE `manage` (
-  `manager_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
-
---
--- Data for `manage`
---
-
-INSERT INTO `manage` (`manager_id`, `username`, `email`, `password`) VALUES 
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES 
 (420, 'admin', 'admin@suot.com', 'admin');
 
 --
--- manage.php
+++ manage.php
@@ -12,10 +12,25 @@
                 <?php 
                         include "inc_files/header.inc"; 
                         include "inc_files/nav.inc";
+                        require_once "settings.php";
                 ?>
 
+                <?php
+                        if (isset($_GET['eoi'])) {
+                                $eoi = mysqli_read_escape_string($conn, $_GET['eoi']);
+                                $sql = "SELECT * FROM eoi";
+                                $result = mysqli_query($conn, $sql);
 
-
+                                if (mysqli_num_rows($result) > 0){
+                                        echo "<table border='1' cellpadding='5'>";
+                                        echo "<tr><th>EOI ID</th><th>First Name</th><th>Preferred Name</th><th>Last Name</th><th>DOB</th><th>Gender</th><th>Street Address</th><th>Suburb</th><th>State</th><th>Postcode</th><th>Job Title</th><th>Job Reference</th><th>Mobile Number</th><th>Home Number</th><th>Email</th><th>Main Skills</th><th>Other Skills</th><th>Experience</th><th>Status</th>";
+                                        while ($row = mysqli_num_rows($result) > 0) {
+                                                echo "<tr>";
+                                                echo "<th>" . $row['eoi_id'] . "</td>";
+                                        }
+                                }
+                        }
+                ?>
 
                 <?php
                         include "inc_files/footer.inc";
