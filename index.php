48fb1f668894ae2c18de9e6719b666bc4979d5ab
Ciara Smith <106510563@student.swin.edu.au>
2026-05-27 17:45:50 +1000

    more faqs

--- index.php
+++ index.php
@@ -51,14 +51,20 @@
                 <div id="faq-content" hidden>
                     Where can I contact for inquiries regarding a specific subject?
                     Our email is avaliable for any inquiries about anything you can think of (info@linkly.com)<br>Otherwise, you can call our Australian phone number at +61 XXX-XXX-XXX, or our international phone number at +XX XXX-XXX-XXX.<br>For inquiries regarding job positions, press 1. For inquiries regarding buying off our platform, press 2. For inquiries regarding selling off our platform, press 3.
+                    How do I sell a product on Linkly?
+                    To become an official Linkly seller, follow our guide over on our Facebook, or watch <em>this</em>Youtube video with a more in-depth, visual explanation.
+                    Can I get a refund?
+                    Unfortunately, Linkly can not 100% guarantee a refund without approval from the seller you are buying from, please contact our Customer Support number at +61 XXX-XXX-XXX option 2 for more information regarding your issue. However, we first recommend contacting the seller.
                 </div>
             <details>
                 <summary style="font-size: 25px; text-align: center;"><strong>FAQ</strong></summary>
                 <ol>
                     <li><strong>Where can I contact for inquiries regarding a specific subject?</strong></li>
                     <ul>Our email is avaliable for any inquiries about anything you can think of (info@linkly.com)<br>Otherwise, you can call our Australian phone number at +61 XXX-XXX-XXX, or our international phone number at +XX XXX-XXX-XXX.<br>For inquiries regarding job positions, press 1. For inquiries regarding buying off our platform, press 2. For inquiries regarding selling off our platform, press 3.</ul>
-                    <li><strong></strong></li>
-                    <ul></ul>
+                    <li><strong>How do I sell a product on Linkly?</strong></li>
+                    <ul>To become an official Linkly seller, follow our guide over on our Facebook, or watch <em>this</em>Youtube video with a more in-depth, visual explanation.</ul>
+                    <li><strong>Can I get a refund?</strong></li>
+                    <ul>Unfortunately, Linkly can not 100% guarantee a refund without approval from the seller you are buying from, please contact our Customer Support number at +61 XXX-XXX-XXX option 2 for more information regarding your issue. However, we first recommend contacting the seller.</ul>
                 </ol>
             </details>
             
