899c1a46709d7f02530868c78b14c7238a157eb3
Cheese34-SSD <106503741@student.swin.edu.au>
2026-05-31 19:17:36 +1000

    switches back, the new link didn't work

--- settings.php
+++ settings.php
@@ -2,5 +2,5 @@
 $host = "localhost";         // because XAMPP runs the server locally
 $user = "root";          // default username for XAMPP's MySQL
 $pwd = "";              // default password is empty in XAMPP
-$sql_db = "linkly_db.sql";  // replace with the actual name of your database
+$sql_db = "linkly_db";  // replace with the actual name of your database
 ?>
