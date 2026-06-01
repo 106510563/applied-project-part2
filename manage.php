02b7d9111c1d2f4234ddb2c6211345a3fe4a5472
Cheese34-SSD <106503741@student.swin.edu.au>
2026-06-01 17:19:44 +1000

    hopefully the final bugs

--- manage.php
+++ manage.php
@@ -30,7 +30,7 @@
                                 $sql = "DELETE FROM eoi WHERE job_ref = '$job_ref'";
                                 if (mysqli_query($conn, $sql)) {
                                         $affected = mysqli_affected_rows($conn);
-                                        echo "<p>Deleted: " . $affected . " EOI(s) with job reference (strong)" . htmlspecialchars($job_ref) . "</strong>.</p>";
+                                        echo "<p>Deleted: " . $affected . " EOI(s) with job reference <strong>" . htmlspecialchars($job_ref) . "</strong>.</p>";
                                 } else {
                                         echo "<p>Please provide a job reference to be deleted.</p>";
                                 }
@@ -126,10 +126,6 @@
 
                 <?php
                 //the results table
-                        $eoi = mysqli_real_escape_string($conn, $_GET['eoi']);
-                        $sql = "SELECT * FROM eoi";
-                        $result = mysqli_query($conn, $sql);
-
                         if (mysqli_num_rows($result) > 0){
                                 echo "<table border='1' cellpadding='5'>";
                                 echo "<tr><th>EOI ID</th><th>First Name</th><th>Preferred Name</th><th>Last Name</th><th>DOB</th><th>Gender</th><th>Street Address</th><th>Suburb</th><th>State</th><th>Postcode</th><th>Job Title</th><th>Job Reference</th><th>Mobile Number</th><th>Home Number</th><th>Email</th><th>Main Skills</th><th>Other Skills</th><th>Experience</th><th>Status</th>";
