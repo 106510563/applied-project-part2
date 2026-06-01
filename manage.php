9383ac6f15ff56ee1ffbc9dd488d41013dbed610
Cheese34-SSD <106503741@student.swin.edu.au>
2026-06-01 17:26:04 +1000

    added the end of tr

--- manage.php
+++ manage.php
@@ -128,7 +128,7 @@
                 //the results table
                         if (mysqli_num_rows($result) > 0){
                                 echo "<table border='1' cellpadding='5'>";
-                                echo "<tr><th>EOI ID</th><th>First Name</th><th>Preferred Name</th><th>Last Name</th><th>DOB</th><th>Gender</th><th>Street Address</th><th>Suburb</th><th>State</th><th>Postcode</th><th>Job Title</th><th>Job Reference</th><th>Mobile Number</th><th>Home Number</th><th>Email</th><th>Main Skills</th><th>Other Skills</th><th>Experience</th><th>Status</th>";
+                                echo "<tr><th>EOI ID</th><th>First Name</th><th>Preferred Name</th><th>Last Name</th><th>DOB</th><th>Gender</th><th>Street Address</th><th>Suburb</th><th>State</th><th>Postcode</th><th>Job Title</th><th>Job Reference</th><th>Mobile Number</th><th>Home Number</th><th>Email</th><th>Main Skills</th><th>Other Skills</th><th>Experience</th><th>Status</th></tr>";
                                 while ($row = mysqli_fetch_assoc($result)) {
                                         echo "<tr>";
                                         echo "<td>" . $row['eoi_id'] . "</td>";
