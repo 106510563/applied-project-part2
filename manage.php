555d8e1480eef8b31dd5a6fd187397be4de89279
Ciara Smith <106510563@student.swin.edu.au>
2026-06-01 17:43:36 +1000

    css fixes to media queries

--- manage.php
+++ manage.php
@@ -127,7 +127,7 @@
                 <?php
                 //the results table
                         if (mysqli_num_rows($result) > 0){
-                                echo "<table border='1' cellpadding='5'>";
+                                echo "<table border='1' cellpadding='5' style='right:0px; position:absolute;'>";
                                 echo "<tr><th>EOI ID</th><th>First Name</th><th>Preferred Name</th><th>Last Name</th><th>DOB</th><th>Gender</th><th>Street Address</th><th>Suburb</th><th>State</th><th>Postcode</th><th>Job Title</th><th>Job Reference</th><th>Mobile Number</th><th>Home Number</th><th>Email</th><th>Main Skills</th><th>Other Skills</th><th>Experience</th><th>Status</th></tr>";
                                 while ($row = mysqli_fetch_assoc($result)) {
                                         echo "<tr>";
